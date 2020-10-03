b4d8eb22d1a40eb7e77d5017c9a577341ba627bb
Nagore <reina.nagore@gmail.com>
2020-10-03 13:57:31 +0200

    Add project files.

---
+++ ConsoleChat/MainProgram.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleChat
+{
+    class MainProgram
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Comienzo");
+        }
+    }
+}
